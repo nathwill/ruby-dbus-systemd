17a43b38ad26d85e6e2e6455e8ab720f677ec644
Nathan Williams <nath.e.will@gmail.com>
2016-10-01 08:46:58 -0700

    initial commit

---
+++ lib/dbus/systemd.rb
@@ -0,0 +1,10 @@
+require_relative "dbus/systemd/version"
+require_relative "dbus/systemd/manager"
+require_relative "dbus/systemd/unit"
+require_relative "dbus/systemd/job"
+
+module Dbus
+  module Systemd
+    # Your code goes here...
+  end
+end
---
+++ spec/dbus/systemd_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Dbus::Systemd do
+  it 'has a version number' do
+    expect(Dbus::Systemd::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
