8d8fde598585cd1d4591c31510ffd7c6938c77df
Nathan Williams <nath.e.will@gmail.com>
2016-10-02 01:09:30 -0700

    define respond_to_missing?

--- lib/dbus/systemd/unit.rb
+++ lib/dbus/systemd/unit.rb
@@ -14,7 +14,15 @@ module DBus
       end
 
       def method_missing(name, *args, &blk)
-        @object.send(name, *args, &blk) if @object.respond_to?(name)
+        if @object.respond_to?(name)
+          @object.send(name, *args, &blk)
+        else
+          super
+        end
+      end
+
+      def respond_to_missing?(*args)
+        @object.respond_to?(*args) || super
       end
     end
   end
