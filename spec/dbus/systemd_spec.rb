8ca0a5aec8e6cdbb147532ceb71c5bf07e7d675f
Nathan Williams <nath.e.will@gmail.com>
2016-10-03 18:27:43 -0700

    stub spec files

--- spec/dbus/systemd_spec.rb
+++ spec/dbus/systemd_spec.rb
@@ -5,17 +5,3 @@ describe DBus::Systemd do
     expect(DBus::Systemd::VERSION).not_to be nil
   end
 end
-
-describe DBus::Systemd::Helpers do
-  let(:raw_array) do
-    ["Fedora-23", "raw", false, 1446171608000000, 1446171608000000, 589676544, "/org/freedesktop/machine1/image/Fedora_2d23"]
-  end
-
-  let(:converted) do
-    {:name=>"Fedora-23", :type=>"raw", :read_only=>false, :creation_time=>1446171608000000, :modification_time=>1446171608000000, :disk_space=>589676544, :object_path=>"/org/freedesktop/machine1/image/Fedora_2d23"}
-  end
-
-  it 'maps positional array to named hash' do
-    expect(DBus::Systemd::Helpers.map_array(raw_array, DBus::Systemd::Machined::Manager::IMAGE_INDICES)).to eq converted
-  end
-end
